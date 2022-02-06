3df9cd5151e3f50c887ee65992c677fcccf9e1a1
william owalla <william.owalla@student.moringaschool.com>
2022-02-06 17:29:29 +0300

    ft: added a dispaly feature

---
+++ index.js
@@ -0,0 +1,6 @@
+function launchExams(){
+    var examForm = document.getElementById("question");
+    examForm.style.display = "block";
+    var landingPage = document.getElementById("container");
+    landingPage.style.display = "none"
+}
